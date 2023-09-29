58e7c3163da20cc561e8a5191ed59478add28b00
mahendra <itsmahendramohane11@gmail.com>
2023-09-29 11:03:54 +0530

    ok

--- index.js
+++ index.js
@@ -1,66 +1,31 @@
-(function() {
-let i=0;
-let txt="Full Stack Web Developer Focused on NodeJS Backend."; //Developer.//
-let speed=120;
+let menu = document.querySelector("#menu-icon");
+let navlist = document.querySelector(".navlist");
 
-typeWriter()
+menu.onclick = () => {
+  menu.classList.toggle("bx-x");
+  navlist.classList.toggle("open");
+};
 
-function typeWriter() {
-    if(i < txt.length){
-      document.getElementById("user-detail-name").style.color="white";
-      document.getElementById("demo1").innerHTML += txt.charAt(i);
-      i++;
-      setTimeout(typeWriter, speed);
-    }
-    else {
-      // reset the index when it reaches the end of the text
-      i = 0;
-      // clear the text
-      document.getElementById("demo1").innerHTML = "";
-      // make the recursive call
-      document.getElementById("user-detail-name").style.color="#f51cb3";
-      setTimeout(typeWriter, speed);
-    }
-  }
+window.onscroll = () => {
+  menu.classList.remove("bx-x");
+  navlist.classList.remove("open");
+};
 
-  
-  var myNav = document.getElementById('nav-menu');
-  var myNav2 = document.getElementById('main');
-  window.onscroll = function () { 
-      if ( document.documentElement.scrollTop >= 15 ) {
-          myNav.classList.add("nav-colored");
-          myNav2.classList.add("nav-colored");
-          // myNav.classList.remove("nav-transparent");
-      } 
-      else {
-          // myNav.classList.add("nav-transparent");
-          myNav.classList.remove("nav-colored");
-          myNav2.classList.remove("nav-colored");
-      }
-  };
+GitHubCalendar(".calendar", "Mahendra-mohane", {
+  responsive: true,
+  global_stats: false,
+  tooltips: true,
+});
 
-  GitHubCalendar(".calendar", "Mahendra Mohane", {
-    responsive: true,
-    global_stats: false,
-    tooltips: true,
-  });
+document.getElementById("resume-link-1").onclick = () => {
+  window.open(
+    "https://drive.google.com/file/d/1PygiTl_pNZV9hhyXPc9YOAz_5QkBzRLm/view?usp=sharing"
+  );
+};
 
-  document.getElementById("resume-button-1").onclick=()=>{
-window.open("./images/Mahendra-Mohane-Resume.pdf");
-  }
 
-  document.getElementById("resume-button-2").onclick=()=>{
-    window.open("./images/Mahendra-Mohane-Resume.pdf");
-  }
-     
-  
-  function openNav() {
-    document.getElementById("mySidebar").style.width = "250px";
-    document.getElementById("main").style.display = "none";
-  }
-  
-  function closeNav() {
-    document.getElementById("mySidebar").style.width = "0";
-    document.getElementById("main").style.display= "block";
-  }
-})();
+document.getElementById("resume-link-2").onclick = () => {
+  window.open(
+    "https://drive.google.com/file/d/1PygiTl_pNZV9hhyXPc9YOAz_5QkBzRLm/view"
+  );
+};
